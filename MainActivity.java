698239b94909c5ccae78ff255875bca880b41cd3
DikerR25 <93419907+DikerR25@users.noreply.github.com>
2022-10-04 13:53:09 +0700

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.dikri_mahali_ramdani_tugas;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button NEXT;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        NEXT = findViewById(R.id.buttonnext);
+        NEXT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,MainActivity2.class));
+            }
+        });
+    }
+}
